f513facd4969748c273bdf3aafec19f48398f9c6
BrunoVarg <brunovargas7899@gmail.com>
2023-11-21 15:08:47 -0300

    feat/api-pets

--- repet-back/polls/views.py
+++ repet-back/polls/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
-from .serializers import UserSerializer
-from .models import Users
+from .serializers import UserSerializer, PetSerializer
+from .models import Users, Pets
 
 # Create your views here.
 class UserView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = Users.objects.all()
+
+class PetView(viewsets.ModelViewSet):
+    serializer_class = PetSerializer
+    queryset = Pets.objects.all()
 
 def index(request):
     return HttpResponse("Teste.")
