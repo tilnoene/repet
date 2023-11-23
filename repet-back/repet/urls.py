a95450b48aa0ff06d9a15191f8e52fb5520bded3
Victor Manuel <mbvictorsantos@gmail.com>
2023-11-23 17:29:15 -0300

    fix: Remove duplicated lines from urls.py

--- repet-back/repet/urls.py
+++ repet-back/repet/urls.py
@@ -9,5 +9,3 @@ urlpatterns = [
     path('users/<int:pk>/', UserView.as_view()),
     # path('pets/', PetView.as_view()),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
