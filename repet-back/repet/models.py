afe8ab1458cb86cad01546a6bed591f516974f7c
BrunoVarg <brunovargas7899@gmail.com>
2023-11-27 16:32:51 -0300

    feat: adding a new json features

--- repet-back/repet/models.py
+++ repet-back/repet/models.py
@@ -6,6 +6,7 @@ class Users(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, verbose_name='Nome')
     email = models.EmailField()
+    image = models.BinaryField(verbose_name='Imagem do Usuário', blank=True)
     username = models.CharField(max_length=100, verbose_name='Nome de Usuário')
     created_at = models.DateField(auto_now_add=True, blank=True, verbose_name='Criado em')
 
@@ -15,11 +16,13 @@ class Users(models.Model):
 
 class Pets(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name='ID do usuário')
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name='ID do usuário')
     name = models.CharField(max_length=200, verbose_name='Nome')
 
-    genders = [('M', 'Macho'), ('F', 'Fêmea')]
-    gender = models.CharField(max_length=1, choices=genders, verbose_name='Gênero')
+    image = models.BinaryField(verbose_name='Imagem do PET', blank=True)
+
+    genders = [('Macho', 'Macho'), ('Fêmea', 'Fêmea')]
+    gender = models.CharField(max_length=5, choices=genders, verbose_name='Gênero', blank=True)
 
     typers = [('Cachorro','Cachorro'),
               ('Gato', 'Gato'),
@@ -30,9 +33,9 @@ class Pets(models.Model):
               ('Outros', 'Outros')]
     type = models.CharField(max_length=8, choices=typers, verbose_name='Tipo', default="")
 
-    birthdate = models.DateField(verbose_name='Aniversário')
-    breed = models.CharField(max_length=100, verbose_name='Raça')
-    weight = models.IntegerField(verbose_name='Peso')
+    birthdate = models.DateField(verbose_name='Aniversário', blank=True)
+    breed = models.CharField(max_length=100, verbose_name='Raça', blank=True)
+    weight = models.IntegerField(verbose_name='Peso', blank=True)
     created_at = models.DateField(auto_now_add=True, blank=True, verbose_name='Criado em')
 
     def __str__(self):
@@ -40,11 +43,11 @@ class Pets(models.Model):
 
 class Records(models.Model):
     id = models.AutoField(primary_key=True)
-    pet_id = models.ForeignKey(Pets, on_delete=models.CASCADE, verbose_name='ID do PET')
+    pet = models.ForeignKey(Pets, on_delete=models.CASCADE, verbose_name='ID do PET')
     title = models.CharField(max_length=50, verbose_name='Título')
-    description = models.TextField(verbose_name='Descrição')
-    date = models.DateField(verbose_name='Data')
-    time = models.TimeField(verbose_name='Horário')
+    description = models.TextField(verbose_name='Descrição', blank=True)
+    date = models.DateField(verbose_name='Data', blank=True)
+    time = models.TimeField(verbose_name='Horário', blank=True)
     created_at = models.DateField(auto_now_add=True, blank=True, verbose_name='Criado em')
 
     def __str__(self):
@@ -52,23 +55,23 @@ class Records(models.Model):
 
 class Vaccine(models.Model):
     id = models.AutoField(primary_key=True)
-    record_id = models.ForeignKey(Records, on_delete=models.CASCADE, verbose_name='ID da Record')
-    pet_id = models.ForeignKey(Pets, on_delete=models.CASCADE, verbose_name='ID do PET')
-    name_vaccine = models.CharField(max_length=100, verbose_name='Vacina', default=None)
-    veterinarian = models.CharField(max_length=100, verbose_name='Veterinário(a)')
-    place = models.CharField(max_length=100, verbose_name='Local')
-    vaccine_card = models.BinaryField(verbose_name='Cartão de Vacina')
+    record = models.ForeignKey(Records, on_delete=models.CASCADE, verbose_name='ID da Record')
+    pet = models.ForeignKey(Pets, on_delete=models.CASCADE, verbose_name='ID do PET')
+    veterinarian = models.CharField(max_length=100, verbose_name='Veterinário(a)', blank=True)
+    place = models.CharField(max_length=100, verbose_name='Local', blank=True)
+    vaccine_card = models.BinaryField(verbose_name='Cartão de Vacina', blank=True)
 
     def __str__(self):
         return f"{self.id}-{self.pet_id}:{self.record_id}"
 
 class Reminders(models.Model):
     id = models.AutoField(primary_key=True)
-    pet_id = models.ForeignKey(Pets, on_delete=models.CASCADE, verbose_name='ID do PET')
+    pet = models.ForeignKey(Pets, on_delete=models.CASCADE, verbose_name='ID do PET')
     title = models.CharField(max_length=100, verbose_name='Título')
-    description = models.TextField(verbose_name='Descrição')
-    date = models.DateField(verbose_name='Data')
-    time = models.TimeField(verbose_name='Horário')
+    description = models.TextField(verbose_name='Descrição', blank=True)
+    color = models.TextField(verbose_name='Cor', blank=True)
+    date = models.DateField(verbose_name='Data', blank=True)
+    time = models.TimeField(verbose_name='Horário', blank=True)
     created_at = models.DateField(auto_now_add=True, blank=True, verbose_name='Criado em')
 
     def __str__(self):
