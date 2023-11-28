b7231776db0fcb254f4e6af50d2311bf78ef346c
BrunoVarg <brunovargas7899@gmail.com>
2023-11-28 16:08:26 -0300

    feat: tryng to deploy in main with static

--- repet-back/repet_back/settings.py
+++ repet-back/repet_back/settings.py
@@ -146,5 +146,5 @@ CORS_ORIGIN_WHITELIST = [
     'https://azulniano.vercel.app'
 ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- repet-back/repet_back/urls.py
+++ repet-back/repet_back/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path("", include("repet.urls")),
     path("admin/", admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
