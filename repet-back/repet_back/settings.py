7705568fdcfdeae523739d522ec71552381134ac
BrunoVarg <brunovargas7899@gmail.com>
2023-11-28 16:14:34 -0300

    feat: modifying settings.py

--- repet-back/repet_back/settings.py
+++ repet-back/repet_back/settings.py
@@ -146,5 +146,7 @@ CORS_ORIGIN_WHITELIST = [
     'https://azulniano.vercel.app'
 ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
