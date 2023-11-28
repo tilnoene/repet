3e90ae2de646d90b2194414745ad7ea519e78a79
BrunoVarg <brunovargas7899@gmail.com>
2023-11-28 15:46:19 -0300

    fix: fixing deploy

--- repet-back/repet_back/settings.py
+++ repet-back/repet_back/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0@e5pczj5bl7v*0o^+oz_nxk3d9+**7)b1zmg!qp4=@qp30v2r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -141,5 +141,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    'http://localhost:3000',
+    'https://repete.vercel.app',
+    'https://azulniano.vercel.app'
 ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
