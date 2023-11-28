099f7ff91dc67a7565ea4a8d3d314f3ad06ac852
BrunoVarg <brunovargas7899@gmail.com>
2023-11-28 15:27:10 -0300

    fix: fixing deploy back

--- repet-back/repet_back/urls.py
+++ repet-back/repet_back/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
