3241fd4835f4506c51976a32e1fc44a87d4d2a03
Victor Manuel <mbvictorsantos@gmail.com>
2023-11-30 18:13:23 -0300

    feat: Create the Input Image component

--- repet-front/src/services/utils.ts
+++ repet-front/src/services/utils.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { ToastOptions } from 'react-toastify';
 
 export const petTypeOptions = [
@@ -35,4 +36,37 @@ export const formatWeight = (weight: string) => {
 
 export const formatTime = (time: string) => {
   return time; // TODO: implementar
+};
+
+export const getAge = (date: string) => {
+  const oldDate = dayjs(date, 'YYYY-MM-DD');
+  const currentDate = dayjs();
+
+  let value;
+  let measurementUnit;
+
+  if (currentDate.diff(oldDate, 'year') > 0) {
+    value = currentDate.diff(oldDate, 'year');
+    measurementUnit = 'ano';
+
+    if (value !== 1) {
+      measurementUnit += 's';
+    }
+  } else if (currentDate.diff(oldDate, 'month') > 0) {
+    value = currentDate.diff(oldDate, 'month');
+    measurementUnit = 'mes';
+
+    if (value !== 1) {
+      measurementUnit += 'es';
+    }
+  } else {
+    value = currentDate.diff(oldDate, 'day');
+    measurementUnit = 'dia';
+
+    if (value !== 1) {
+      measurementUnit += 's';
+    }
+  }
+
+  return `${value} ${measurementUnit}`;
 };
