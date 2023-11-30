d87316e351226c0dd296bc2e8a29eb2ebea1a39a
Victor Manuel <mbvictorsantos@gmail.com>
2023-11-30 16:31:09 -0300

    fix: Remove csrf

--- repet-back/repet_back/settings.py
+++ repet-back/repet_back/settings.py
@@ -49,19 +49,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
-
-''' TODO: adicionar por segurança
-CORS_ALLOWED_ORIGINS = [
-    'https://repete.vercel.app',
-]
-'''
 
 ROOT_URLCONF = 'repet_back.urls'
 
