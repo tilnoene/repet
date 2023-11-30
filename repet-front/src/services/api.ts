25b0c8abb64426e035c8ad9fab0960ee5af0e9c4
Victor Manuel <mbvictorsantos@gmail.com>
2023-11-30 19:39:53 -0300

    fix: Fix style changing display flow to block

--- repet-front/src/services/api.ts
+++ repet-front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://repet-back-csrf.vercel.app/', // http://localhost:4000
+  baseURL: 'https://repet-back.vercel.app/', // http://localhost:4000
   headers: {
     'Content-Type': 'application/json',
   },
