f0b05b85b33510b0e5089d19bf75125ce8c94a7e
BrunoVarg <brunovargas7899@gmail.com>
2023-12-01 21:12:30 -0300

    feat: login with email too

--- repet-back/repet/views.py
+++ repet-back/repet/views.py
@@ -24,6 +24,10 @@ class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
+        
+        if '@' in request.data['username']:
+            request.data['username'] = User.objects.get(email=request.data['username']).username
+            
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
