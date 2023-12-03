bfef4d165ed235aedc2cd87fd4b221b9be36e715
Victor Manuel <mbvictorsantos@gmail.com>
2023-12-03 02:06:02 -0300

    feat: Remove scrollbar and center content and limit to 1024px

--- repet-front/src/components/Page/styles.ts
+++ repet-front/src/components/Page/styles.ts
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import config from '../../config.json';
 
 export const ContainerPage = styled.div`
+  display: flex;
+  justify-content: center;
+
   width: 100%;
   max-height: 100vh;
   background-color: ${config.colors.background};
@@ -14,6 +17,7 @@ type ContentProps = {
 };
 
 export const Content = styled.div<ContentProps>`
+  max-width: 1024px;
   width: 100%;
   height: calc(100vh - 60px - 60px);
   margin-top: 60px;
