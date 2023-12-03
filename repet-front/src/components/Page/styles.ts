9bcff2b7a61101672503054971abccb4490a8d41
Victor Manuel <mbvictorsantos@gmail.com>
2023-12-03 01:21:02 -0300

    fix: Fix scroll on mobile devices

--- repet-front/src/components/Page/styles.ts
+++ repet-front/src/components/Page/styles.ts
@@ -5,23 +5,24 @@ import config from '../../config.json';
 export const ContainerPage = styled.div`
   width: 100%;
   max-height: 100vh;
-  background-color: ${ config.colors.background };
+  background-color: ${config.colors.background};
 `;
 
 type ContentProps = {
   centered: boolean;
   padding: boolean;
-}
+};
 
 export const Content = styled.div<ContentProps>`
   width: 100%;
   height: calc(100vh - 60px - 60px);
   margin-top: 60px;
   overflow: scroll;
-  padding: ${ props => props.padding ? '18px' : '0' };
+  padding: ${props => (props.padding ? '18px' : '0')};
   overflow-x: hidden;
-  display: ${ props => props.centered ? 'flex' : 'block' };
+  display: ${props => (props.centered ? 'flex' : 'block')};
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding-bottom: calc(60px + 18px);
 `;
