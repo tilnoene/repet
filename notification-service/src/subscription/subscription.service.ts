bd240b8737083dfc85db0e483a3f4e6183a1bb7f
Victor Manuel <mbvictorsantos@gmail.com>
2023-12-04 15:06:53 -0300

    fix: Removing cache form vercel

--- notification-service/src/subscription/subscription.service.ts
+++ notification-service/src/subscription/subscription.service.ts
@@ -24,20 +24,4 @@ export class SubscriptionService {
       throw new ForbiddenException(error);
     }
   }
-
-  // findAll() {
-  //   return `This action returns all subscription`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} subscription`;
-  // }
-
-  // update(id: number, updateSubscriptionDto: UpdateSubscriptionDto) {
-  //   return `This action updates a #${id} subscription`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} subscription`;
-  // }
 }
