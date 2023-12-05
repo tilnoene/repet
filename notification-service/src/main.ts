e2e1ea6773d2dd92e400ed14d9e31b8805caed74
BrunoVarg <brunovargas7899@gmail.com>
2023-12-04 21:11:55 -0300

    fix: tryinf to fix cors with another module

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-      origin: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-  });
+  app.enableCors();
   await app.listen(4001);
 }
 bootstrap();
