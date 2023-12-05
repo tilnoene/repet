e2a195499e1d7ba85df4173f492e8e2a57016709
BrunoVarg <brunovargas7899@gmail.com>
2023-12-04 21:06:58 -0300

    fix: trying to fix cors

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
   });
   await app.listen(4001);
 }
